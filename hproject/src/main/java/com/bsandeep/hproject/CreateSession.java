372f44132047d6e1d4474ab7d545e58b8052ffed
Radium Raja <sb2017ess@gmail.com>
2020-02-23 16:29:35 +0530

    FirstCommit

---
+++ hproject/src/main/java/com/bsandeep/hproject/CreateSession.java
@@ -0,0 +1,55 @@
+package com.bsandeep.hproject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateSession
+ */
+@WebServlet({ "/CreateSession", "/cs" })
+public class CreateSession extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private String getSessionKey(String username, String password) {
+		
+		String returnStr = null;
+		
+		returnStr = username+password;
+		
+		return returnStr;
+		
+	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateSession() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("This servlet only accepts post request").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("uname");
+		String password = request.getParameter("pass");
+		
+		response.getWriter().append("SessionKey:"+getSessionKey(username, password));
+		
+	}
+
+}
